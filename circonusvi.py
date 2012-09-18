9b76afaf2f79d4c060528f12278bb19fe04533f6
Mark Harrison <mark@mivok.net>
2012-09-18 16:18:13 -0400

    Reorganize everything into functions instead of one giant stream of code

--- circonusvi.py
+++ circonusvi.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import collections
 import ConfigParser
 import getopt
 import json
@@ -35,116 +36,121 @@ def confirm(text="OK to continue?"):
         return True
     return False
 
-account = config.get('general', 'default_account', None)
-debug = False
-endpoints = []
-editor = os.environ.get('EDITOR', 'vi')
-try:
-    opts, args = getopt.gnu_getopt(sys.argv[1:], "a:de:E:?")
-except getopt.GetoptError, err:
-    # print help information and exit:
-    print str(err) # will print something like "option -a not recognized"
-    usage()
-    sys.exit(2)
+options = {
+    'account': config.get('general', 'default_account', None),
+    'debug': False,
+    'endpoints': [],
+    'editor': os.environ.get('EDITOR', 'vi')
+}
 
-for o,a in opts:
-    if o == '-a':
-        account = a
-    if o == '-d':
-        debug = not debug
-    if o == '-e':
-        endpoints.append(a)
-    if o == '-E':
-        editor = a
-    if o == '-?':
-        usage()
-        sys.exit(0)
-
-token = config.get('tokens', account, None)
-api = circonusapi.CirconusAPI(token)
-if debug:
-    api.debug = True
-
-if not endpoints:
-    # user, account are missing from here
-    # TODO - it's probably a good idea to pick only commonly used endpoints by
-    # default here, or perhaps make it configurable.
-    endpoints = ['broker', 'check_bundle', 'contact_group', 'graph',
-            'rule_set', 'template', 'worksheet']
-
-# Combined output to be serialized
-data = {}
-
-for t in endpoints:
-    data.update(dict(((i['_cid'], i) for i in api.api_call("GET", t))))
-
-# Filter based on the pattern
-patterns = []
-for p in args:
-    parts = p.split('=', 1)
-    if len(parts) != 2:
-        print "Invalid pattern: %s" % p
+def parse_options():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "a:de:E:?")
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
-    patterns.append(parts)
 
-filtered_data = {}
-for i in data:
-    matched = True
-    for k, p in patterns:
-        if k not in data[i]:
-            break
-        if not re.search(p, data[i][k]):
-            break
-    else:
-        filtered_data[i] = data[i]
-data = filtered_data
+    for o,a in opts:
+        if o == '-a':
+            options['account'] = a
+        if o == '-d':
+            options['debug'] = not options['debug']
+        if o == '-e':
+            options['endpoints'].append(a)
+        if o == '-E':
+            options['editor'] = a
+        if o == '-?':
+            usage()
+            sys.exit(0)
+    return args
 
-tmp = tempfile.mkstemp(suffix='.json')
-fh = os.fdopen(tmp[0], 'w')
-json.dump(data, fh, sort_keys=True, indent=4, separators=(',',': '))
-fh.close()
+def get_api():
+    token = config.get('tokens', options['account'], None)
+    api = circonusapi.CirconusAPI(token)
+    if options['debug']:
+        api.debug = True
+    return api
 
-ok = False
-while not ok:
-    subprocess.call([editor, tmp[1]])
+def get_circonus_data(api, args):
+    if not options['endpoints']:
+        # user, account are missing from here TODO - it's probably a good idea
+        # to pick only commonly used endpoints by default here, or perhaps
+        # make it configurable.
+        options['endpoints'] = ['broker', 'check_bundle', 'contact_group',
+                'graph', 'rule_set', 'template', 'worksheet']
 
-    fh = open(tmp[1])
-    try:
-        data_new = json.load(fh)
-        ok = True
-    except ValueError, e:
-        print "Error parsing JSON:", e
-        if not confirm("Do you want to edit the file again?"):
-            sys.exit(1)
+    # Combined output to be serialized
+    data = {}
+
+    for t in options['endpoints']:
+        data.update(dict(((i['_cid'], i) for i in api.api_call("GET", t))))
+
+    # Filter based on the pattern
+    patterns = []
+    for p in args:
+        parts = p.split('=', 1)
+        if len(parts) != 2:
+            print "Invalid pattern: %s" % p
+            usage()
+            sys.exit(2)
+        patterns.append(parts)
+
+    filtered_data = {}
+    for i in data:
+        for k, p in patterns:
+            if k not in data[i]:
+                break
+            if not re.search(p, data[i][k]):
+                break
+        else:
+            filtered_data[i] = data[i]
+    return filtered_data
+
+def edit_file(data):
+    tmp = tempfile.mkstemp(suffix='.json')
+    fh = os.fdopen(tmp[0], 'w')
+    json.dump(data, fh, sort_keys=True, indent=4, separators=(',',': '))
     fh.close()
 
-os.remove(tmp[1])
+    ok = False
+    while not ok:
+        subprocess.call([options['editor'], tmp[1]])
 
-changes = []
-additions = 0
-deletions = 0
-edits = 0
-for i in data_new:
-    if i not in data:
-        # Addition
-        # We need to make sure the endpoint doesn't contain an ID (just in
-        # case the entry contains one)
-        changes.append({'action': 'POST', 'data': data_new[i],
-            'endpoint': re.sub("(?!^)/.*", "", i)})
-        additions += 1
-    elif data[i] != data_new[i]:
-        # Edit
-        changes.append({'action': 'PUT', 'data': data_new[i], 
-            'data_old': data[i], 'endpoint': i})
-        edits += 1
-for i in data:
-    if i not in data_new:
-        # Delete
-        changes.append({'action': 'DELETE', 'data': data[i], 'endpoint': i})
-        deletions += 1
+        fh = open(tmp[1])
+        try:
+            data_new = json.load(fh)
+            ok = True
+        except ValueError, e:
+            print "Error parsing JSON:", e
+            if not confirm("Do you want to edit the file again?"):
+                sys.exit(1)
+        fh.close()
 
-def show_changes():
+    os.remove(tmp[1])
+    return data_new
+
+def calculate_changes(data, data_new):
+    changes = []
+    for i in data_new:
+        if i not in data:
+            # Addition
+            # We need to make sure the endpoint doesn't contain an ID (just in
+            # case the entry contains one)
+            changes.append({'action': 'POST', 'data': data_new[i],
+                'endpoint': re.sub("(?!^)/.*", "", i)})
+        elif data[i] != data_new[i]:
+            # Edit
+            changes.append({'action': 'PUT', 'data': data_new[i], 
+                'data_old': data[i], 'endpoint': i})
+    for i in data:
+        if i not in data_new:
+            # Delete
+            changes.append({'action': 'DELETE', 'data': data[i], 'endpoint': i})
+    return changes
+
+def show_changes(changes):
     pretty_actions = {
         'DELETE': "Delete",
         'POST': "Addition",
@@ -174,36 +180,52 @@ def show_changes():
             print json.dumps(c['data'], sort_keys=True,
                     indent=4, separators=(',',': '))
 
-if not changes:
-    print "No Changes. Exiting."
-    sys.exit(1)
+def confirm_changes(changes):
+    if not changes:
+        print "No Changes. Exiting."
+        sys.exit(1)
 
-print "%d additions, %d deletions, %d edits" % (additions, deletions, edits)
-response = None
-while True:
-    response = raw_input("Do you want to proceed? (YyNnSs?) ")
-    if response in ['Y', 'y']:
-        break
-    if response in ['N', 'n']:
+    counts = collections.Counter((c['action'] for c in changes))
+
+    print "%d additions, %d deletions, %d edits" % (
+            counts['POST'], counts['DELETE'], counts['PUT'])
+    response = None
+    while True:
+        response = raw_input("Do you want to proceed? (YyNnSs?) ")
+        if response in ['Y', 'y']:
+            return True
+        if response in ['N', 'n']:
+            return False
+        if response in ['S', 's']:
+            show_changes(changes)
+        if response in ['?']:
+            print "Y - Proceed"
+            print "N - Quit"
+            print "S - Show changes"
+            print "? - Help"
+
+def make_changes(changes):
+    for c in changes:
+        print "Making API Call: %s %s ..." % (c['action'], c['endpoint']),
+        if c['action'] == 'DELETE':
+            # We don't send any data along for deletions
+            c['data'] = None
+        try:
+            api.api_call(c['action'], c['endpoint'], c['data'])
+        except circonusapi.CirconusAPIError, e:
+            print "Error"
+            print "    %s" % e
+            continue
+        print "Success"
+
+if __name__ == '__main__':
+    args = parse_options()
+    api = get_api()
+    data = get_circonus_data(api, args)
+    data_new = edit_file(data)
+    changes = calculate_changes(data, data_new)
+    if confirm_changes(changes):
+        make_changes(changes)
+    else:
         print "Exiting"
         sys.exit(1)
-    if response in ['S', 's']:
-        show_changes()
-    if response in ['?']:
-        print "Y - Proceed"
-        print "N - Quit"
-        print "S - Show changes"
-        print "? - Help"
-
-for c in changes:
-    print "Making API Call: %s %s ..." % (c['action'], c['endpoint']),
-    if c['action'] == 'DELETE':
-        # We don't send any data along for deletions
-        c['data'] = None
-    try:
-        api.api_call(c['action'], c['endpoint'], c['data'])
-    except circonusapi.CirconusAPIError, e:
-        print "Error"
-        print "    %s" % e
-        continue
-    print "Success"
